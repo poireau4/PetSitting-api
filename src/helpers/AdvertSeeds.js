4ca7fefd53266a4daf6602ddf616a09c692392a0
poireau4 <32650287+poireau4@users.noreply.github.com>
2017-11-07 21:32:25 +0100

    Update AdvertSeeds.js
    added activated

--- src/helpers/AdvertSeeds.js
+++ src/helpers/AdvertSeeds.js
@@ -8,6 +8,7 @@ export default [
     //image: "https://cdn.video.nationalgeographic.com/dims4/default/d91fe48/2147483647/thumbnail/354x199%3E/quality/90/?url=https%3A%2F%2Fpmdvod.nationalgeographic.com%2FNG_Video%2F980%2F231%2F44209_1_1280x720_640x360_177546819636.jpg",
     date: "2017-10-30",
     userId: "0",
-    petId: "0"
+    petId: "0",
+    activated: true
   }
 ];
